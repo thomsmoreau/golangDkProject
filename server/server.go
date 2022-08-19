bed19a07be57d32c126fa056b47542557b3036c1
thomasmoreau <thomas.moreau947@gmail.com>
2022-08-19 02:53:15 +0200

    feat: first commit, adding all necessary function for server side, adding integration test

--- server/server.go
+++ server/server.go
@@ -1,2 +1,76 @@
-// Package server contains everything for setting up and running the HTTP server.
+/*
+Package server contains everything for setting up and running the HTTP server.
+*/
 package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Server struct {
+	address string
+	mux     chi.Router
+	server  *http.Server
+}
+
+type Options struct {
+	Host string
+	Port int
+}
+
+// New turns the host and port pair into an address the server understands (in the form host:port)
+//
+//	and creates a http.Server using that address and the mux.
+func New(opts Options) *Server {
+	// Join the host and port with a colon.
+	address := net.JoinHostPort(opts.Host, strconv.Itoa(opts.Port))
+	// Create a new router.
+	mux := chi.NewMux()
+	// Create a new server.
+	return &Server{
+		address: address,
+		mux:     mux,
+		server: &http.Server{
+			Addr:              address,
+			Handler:           mux,
+			ReadTimeout:       5 * time.Second,
+			ReadHeaderTimeout: 5 * time.Second,
+			WriteTimeout:      5 * time.Second,
+			IdleTimeout:       5 * time.Second,
+		},
+	}
+}
+
+// Start the Server by setting up routes and listening for HTTP requests on the given address.
+func (s *Server) Start() error {
+	s.setupRoutes()
+
+	log.Println("Starting on", s.address)
+	if err := s.server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return fmt.Errorf("error starting server: %w", err)
+	}
+	return nil
+}
+
+// Stop the Server gracefully within the timeout.
+func (s *Server) Stop() error {
+	log.Println("Stopping")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := s.server.Shutdown(ctx); err != nil {
+		return fmt.Errorf("error stopping server: %w", err)
+	}
+
+	return nil
+}
