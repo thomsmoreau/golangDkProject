4f40f736fdc2e5357947de9d0a86e91f516dea9b
thomasmoreau <thomas.moreau947@gmail.com>
2022-11-09 00:53:12 +0100

    fix: typo

--- handlers/health.go
+++ handlers/health.go
@@ -9,7 +9,7 @@ import (
 func Health(mux chi.Router) {
 	/*
 		Pass mux directly to facilitate tests, route closer to function
-		our URL path often contains variables, nice to be able to see the name close to the code
+		URL's path often contains variables, nice to be able to see the name close to the code
 	*/
 	// Default to returning HTTP 200 OK if nothing else is set
 	mux.Get("/health", func(w http.ResponseWriter, r *http.Request) {
